dd13b1f265ffb3a28c921e497a759278e73a77b0
KAwfang <jitlada.yt@gmail.com>
2025-03-03 15:52:12 +0700

    modify email template

--- src/utils/email/ws-templates/rejectEmail.js
+++ src/utils/email/ws-templates/rejectEmail.js
@@ -91,7 +91,6 @@ export function generateRejectionEmail(rejectionDetails) {
     let endTime = endHour.toString().padStart(2, '0') + ':00';
 
     let dateStr = new Date(serviceDate);
-    dateStr.setDate(dateStr.getDate() + 1);
     const options = { day: 'numeric', month: 'long', year: 'numeric' };
     let dateEN = dateStr.toLocaleDateString('en-US', options)
     let dateTH = dateStr.toLocaleDateString('th-TH', options)
