67a6aa1c4ad1fb5dc8a969319ac8f97f1e5b962b
KAwfang <jitlada.yt@gmail.com>
2025-03-04 21:14:11 +0700

    update fetch list ws api

--- src/utils/jwt.js
+++ src/utils/jwt.js
@@ -115,6 +115,8 @@ export const authenticateRequest = async (request, allowedRoles = []) => {
         };
     }
 
+    console.log(user)
+
     // ตรวจสอบสิทธิ์
     if (allowedRoles.length > 0 && !allowedRoles.includes(user.role)) {
         return {
