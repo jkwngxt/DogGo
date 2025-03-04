abe677938f28d0671c7ad4e1eabe17ad44e420bc
chantip-pi <chantip.pi@ku.th>
2025-03-04 20:38:54 +0700

    Add walk-confirm integration to get data

--- src/app/dog-walker/walk-confirm/[wsId]/page.js
+++ src/app/dog-walker/walk-confirm/[wsId]/page.js
@@ -1,9 +1,12 @@
-import React from "react";
+"use client";
+
+import React, { useEffect, useState } from "react";
 import { Card } from "@/components/ui/card";
 import WalkerWalkConfirmation from "@/components/walker-walk-confirmation";
 import DogTable from "@/components/dog-table";
+import Loading from "@/components/loading";
 
-export default function DogWalkConfirm({ wsId}) {
+export default function DogWalkConfirm({ params }) {
    const formatDate = (dateString) => {
       if (!dateString) return "";
       const date = new Date(dateString);
