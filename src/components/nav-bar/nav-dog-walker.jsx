7173a5fcec6aa182a4a6160284a6b135ee36fd06
KAwfang <jitlada.yt@gmail.com>
2025-03-09 00:21:51 +0700

    modify nav bar ui

--- src/components/nav-bar/nav-dog-walker.jsx
+++ src/components/nav-bar/nav-dog-walker.jsx
@@ -42,7 +42,7 @@ const DogWalkerNav = ({ userImage, userName }) => {
 
   return (
     <div className="flex px-10 bg-[#2668E3] justify-between">
-      <img className="w-20 h-20" src="/image/logo.svg" alt="dog go logo" />
+      <img className="w-20 h-20" src="/image/logoInvert.svg" alt="dog go logo"/>
       <div className="flex flex-row space-x-4 items-center">
         <img src={userImage} 
         alt="User profile" 
